05ffe927f4c792eeb0aba7df455f73405b35c5fc
Charliemur2 <charliemurciamunoz@gmail.com>
2020-08-12 02:27:39 +0000

    comment in amenity

--- models/amenity.py
+++ models/amenity.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 
 
 class Amenity(BaseModel):
+    """Amenities of places"""
     __tablename__ = "amenities"
     name = column(string(128), nullable=False)
 
