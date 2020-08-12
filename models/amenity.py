98354ae4360550968bc008d811dde37608e02561
Charliemur2 <charliemurciamunoz@gmail.com>
2020-08-12 03:18:58 +0000

    base reference in imports of classes

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """Amenities of places"""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
