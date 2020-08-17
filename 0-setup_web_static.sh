26accaadc5d80759cfab58a2a6c544c1b958070f
Norman <isaza00@hotmail.com>
2020-08-17 17:21:39 -0500

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,6 @@ apt-get -y install nginx
 mkdir -p /data/web_static/releases/test
 mkdir -p /data/web_static/shared/
 echo "I am working" > /data/web_static/releases/test/index.html
-rm -f /data/web_static/current
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i '/error_page/ i location /hbnb_static \{\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
