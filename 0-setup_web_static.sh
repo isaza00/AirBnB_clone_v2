d5aaf2c0cb1b3afd09a8246d2cc8fdc40a5c5e42
Norman <isaza00@hotmail.com>
2020-08-17 17:32:47 -0500

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,5 +7,7 @@ mkdir -p /data/web_static/shared/
 echo "I am working" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i '/error_page/ i location /hbnb_static \{\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+link_file="/data/web_static/current"
+config="/etc/nginx/sites-enabled/default"
+sed -i "11i \\\\tlocation /hbnb_static/ {\n\t\talias $link_file/;\n\t\tautoindex off;\n\t}\n" "$config"
 service nginx restart
