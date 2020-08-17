0739dd1f33bcca048f698785483e6ab4e8de8ff1
Norman <isaza00@hotmail.com>
2020-08-17 17:56:53 -0500

    all

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,10 +4,10 @@ sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared/
-echo "I am working" > /data/web_static/releases/test/index.html
+echo "I am working" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 link_file="/data/web_static/current"
 config="/etc/nginx/sites-enabled/default"
-sudo sed -i "11i \\\\tlocation /hbnb_static/ {\n\t\talias $link_file/;\n\t\tautoindex off;\n\t}\n" "$config"
+sudo sed -i "41i \\\\tlocation /hbnb_static/ {\n\t\talias $link_file/;\n\t\tautoindex off;\n\t}\n" "$config"
 sudo service nginx restart
